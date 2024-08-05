231d7cc7430840a4673519cad993ef42541bcda5
KalpShahh <kalps328@gmail.com>
2024-08-05 09:41:27 +0530

    qq

--- src/Big.js
+++ src/Big.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import axios from "axios";
 import Button from "@mui/material/Button";
-import { Box, styled,Paper } from "@mui/material";
-
+import { Box, styled, Paper } from "@mui/material";
+import NotInterestedIcon from "@mui/icons-material/NotInterested";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import Table from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
@@ -21,22 +22,27 @@ import CloseIcon from "@mui/icons-material/Close";
 
 const localizer = momentLocalizer(moment);
 const MyCalendar = () => {
-    function convertToIST(utcDateStr) {
-        const date = new Date(utcDateStr);
-      
-        const options = {
-          timeZone: "Asia/Kolkata",
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          // hour12: false // 24-hour format
-        };
-      
-        return new Intl.DateTimeFormat("en-US", options).format(date);
-      }
-      
-  const [date, setdate] = React.useState("");
+  function convertToIST(utcDateStr) {
+    const date = new Date(utcDateStr);
+
+    const options = {
+      timeZone: "Asia/Kolkata",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+
+    return new Intl.DateTimeFormat("en-US", options).format(date);
+  }
+
+  const [date, setDate] = React.useState("");
   const [open, setOpen] = React.useState(false);
+  const [open1, setOpen1] = React.useState(false);
+  
+  const [day, setDay] = React.useState("");
+  const [arr, setArr] = React.useState([]);
+  const [loading, setLoading] = useState(false);
+  const [stu, setstu] = React.useState([]);
   const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     "& .MuiDialogContent-root": {
       padding: theme.spacing(2),
@@ -45,18 +51,17 @@ const MyCalendar = () => {
       padding: theme.spacing(1),
     },
   }));
- 
-  const [day, setday] = React.useState('');
+
   const handleClose = () => {
-    setdate('')
-    setday('')
-
-    setOpen(false);
+    setOpen(() => false);
+    setDate(() => "");
+    setDay(() => "");
+    setArr(() => []);
   };
-  const [arr, setarr] = React.useState([]);
-
+const handleClose1=()=>{
+  setOpen1(() => false);
+}
   function getDayName(dateString) {
-   
     if (!dateString) {
       return "Invalid date";
     }
@@ -66,71 +71,68 @@ const MyCalendar = () => {
     }
 
     return date.toLocaleDateString("en-US", { weekday: "long" });
-  
-}
+  }
 
-  React.useEffect(() => {
-
-    if (date) {
-      console.log('it has date')
+  useEffect(() => {
+    if (open) {
+      setLoading(() => true);
       const dayName = getDayName(date);
-      setday(dayName);
+      setDay(() => dayName);
+      if (day) {
+        axios
+          .get(`http://localhost:5000/Dashboard/CourseData?day=${day}`)
+          .then((data) => {
+            setLoading(() => false);
+            setArr(() => data.data.data);
+            console.log(data);
+          })
+          .catch((err) => {
+            setLoading(() => false);
+            console.log(err);
+          });
+      }
     }
-  }, [date,day]);
-
-  React.useEffect(() => {
-    if (day) {
-      console.log('api called')
-      axios
-        .get(`http://localhost:5000/Dashboard/CourseData?day=${day}`)
-        .then((data) => {
-          console.log(data);
-          
-          setarr(data.data.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, [day]);
+  }, [open, date]);
 
   const handleSelectSlot = (slotInfo) => {
+    setOpen(() => true);
     const selectedDate = new Date(slotInfo.start);
-
-    const timezoneOffset = 5.5 * 60; // 5.5 hours in minutes
+    const timezoneOffset = 5.5 * 60;
     const adjustedDate = new Date(
       selectedDate.getTime() + timezoneOffset * 60 * 1000
     );
     const formattedDate = adjustedDate.toISOString();
     const dayName = getDayName(formattedDate);
-    setdate(formattedDate);
-    setday(dayName);
-    setOpen(true)
-  
+
+    setArr(() => []);
+    setDate(() => formattedDate);
+    setDay(() => dayName);
   };
 
-  console.log('date:',date);
-  console.log('day:',day);
-  console.log('state:',open)
   return (
     <div>
       <Calendar
         localizer={localizer}
         selectable
-        
         startAccessor="start"
         style={{ height: "500px" }}
         onSelectSlot={handleSelectSlot}
       />
+
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
-
+        sx={{
+          "& .MuiDialog-paper": {
+            width: "55%", // Set the width of the dialog
+            maxWidth: "none", // Remove the max-width limit
+          },
+        }}
       >
         <Box>
           <Grid container>
-            <Grid xs={5}></Grid>
+            <Grid item xs={11}></Grid>
             <IconButton
               aria-label="close"
               onClick={handleClose}
@@ -142,135 +144,185 @@ const MyCalendar = () => {
             </IconButton>
           </Grid>
 
-          <Box
-            // sx={{
-            //   display: "flex",
-            //   flexDirection: "column",
-            //   justifyContent: "center",
-            //   alignItems: "center",
-            //   fontSize: 20,
-            // }}
+          <Box>
+            <DialogContent
+              sx={{
+                width: "100%", // Ensure the content takes up full width
+                padding: 0,
+              }}
+            >
+              <Box sx={{ mt: 2 }}>
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell
+                          align="center"
+                          sx={{
+                            position: "sticky",
+                            left: 0,
+                            backgroundColor: "white",
+                          }}
+                        >
+                          Batch Name
+                        </TableCell>
+                        <TableCell align="center">Start Date</TableCell>
+                        <TableCell align="center">Days</TableCell>
+                        <TableCell align="center">Batch Time</TableCell>
+                        <TableCell align="center">Course</TableCell>
+                        <TableCell align="center" colSpan={2}>
+                          Actions
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
 
-          >
-            <DialogContent>
-            <Box sx={{ mx: 2, mt: 2 }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  align="center"
-                  sx={{ position: "sticky", left: 0, backgroundColor: "white" }}
-                >
-                  Batch Name
-                </TableCell>
+                    <TableBody>
+                      {loading ? (
+                        <TableRow>
+                          <TableCell colSpan={6} align="center">
+                            Loading batches...
+                          </TableCell>
+                        </TableRow>
+                      ) : arr.length < 1 ? (
+                        <TableRow>
+                          <TableCell colSpan={6} align="center">
+                            No Batches For Today!
+                          </TableCell>
+                        </TableRow>
+                      ) : (
+                        arr.map((row, index) => (
+                          <TableRow
+                            key={index}
+                            sx={{
+                              "&:last-child td, &:last-child th": { border: 0 },
+                            }}
+                          >
+                            <TableCell
+                              align="center"
+                              sx={{
+                                position: "sticky",
+                                left: 0,
+                                backgroundColor: "white",
+                                zIndex: 1,
+                              }}
+                            >
+                              {row.batchName}
+                            </TableCell>
 
-                <TableCell align="center">Start Date</TableCell>
+                            <TableCell align="center">
+                              {row.StartDate && row.StartDate.split("T")[0]}
+                            </TableCell>
 
-                <TableCell align="center">Days</TableCell>
-                <TableCell align="center">Batch Time</TableCell>
-                <TableCell align="center">Course</TableCell>
-
-                <TableCell align="center"  colSpan={2}>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-
-            {
-               arr.length<1?
-               <TableRow>
-               <TableCell colSpan={6} align="center">
-                 No Batches For Today!
-               </TableCell>
-             </TableRow>
-               
-               :
-
-            arr &&
-              arr.map((row) => (
-                <TableBody sx={{ height: arr && arr.length < 1 ? 200 : 0 }}>
-                  <TableRow
-                    key={row.name}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell
-                      align="center"
-                      sx={{
-                        position: "sticky",
-                        left: 0,
-                        backgroundColor: "white",
-                        zIndex: 1,
-                      }}
-                    >
-                      {row.batchName}
-                    </TableCell>
-
-                    <TableCell align="center">
-                      {row.StartDate && row.StartDate.split("T")[0]}
-                    </TableCell>
-
-                    <TableCell align="center">
-                      {row.Days.map((val, index) => (
-                        <div key={index}>{val}</div>
-                      ))}
-                    </TableCell>
-                    <TableCell align="center">
-                      {row.BatchTime && convertToIST(row.BatchTime)}
-                    </TableCell>
-                    <TableCell align="center">{row.Course}</TableCell>
-                    {/* <TableCell align="center">
-                      <Button
-                        sx={{ color: "black" }}
-                        onClick={() => {
-
-                          
-                          axios
-                          .get(`http://localhost:5000/Dashboard/StudentDetails?courseId=${row._id}`)
-                          .then((data) => {
-                            console.log(data);
-                            data &&
-                              data.data.data.map((val) => {
-                                setstu(val.StuName);
-                            
-                  
-                                
-                              });
-                          })
-                          .catch((err) => {
-                            console.log(err);
-                          });
-                        }}
-                      >
-                        <RemoveRedEyeIcon />
-                      </Button>
-                    </TableCell> */}
-                    {/* <TableCell align="center">
-                    <Button
-                        sx={{ color: "black" }}
-                        onClick={() => {
-                          setstu([])
-                        }}
-                      >
-                        <NotInterestedIcon/>
-                             </Button>
-                    </TableCell> */}
-                  </TableRow>
-                </TableBody>
-              ))
-              
-              
-              }
-
-
-
-          </Table>
-        </TableContainer>
-      </Box>
-
-
+                            <TableCell align="center">
+                              {row.Days.map((val, index) => (
+                                <div key={index}>{val}</div>
+                              ))}
+                            </TableCell>
+                            <TableCell align="center">
+                              {row.BatchTime && convertToIST(row.BatchTime)}
+                            </TableCell>
+                            <TableCell align="center">{row.Course}</TableCell>
+                            <TableCell align="center">
+                              <Button
+                                sx={{ color: "black" }}
+                                onClick={() => {
+                                  axios
+                                    .get(
+                                      `http://localhost:5000/Dashboard/StudentDetails?courseId=${row._id}`
+                                    )
+                                    .then((data) => {
+                                      console.log(data);
+                                      data &&
+                                        data.data.data.map((val) => {
+                                          setstu(val.StuName);
+                                        });
+                                     setOpen1(true)   
+                                    })
+                                    .catch((err) => {
+                                      console.log(err);
+                                    });
+                                }}
+                              >
+                                <RemoveRedEyeIcon />
+                              </Button>
+                            </TableCell>
+                            <TableCell align="center">
+                              <Button
+                                sx={{ color: "black" }}
+                                onClick={() => {
+                                  setstu([]);
+                                }}
+                              >
+                                <NotInterestedIcon />
+                              </Button>
+                            </TableCell>
+                          </TableRow>
+                        ))
+                      )}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
             </DialogContent>
           </Box>
         </Box>
       </BootstrapDialog>
+      <BootstrapDialog
+        onClose={handleClose1}
+        aria-labelledby="customized-dialog-title"
+        open={open1}
+       
+      >
+        <Box>
+          <Grid container>
+            <Grid item xs={10}></Grid>
+            <IconButton
+              aria-label="close"
+              onClick={handleClose1}
+              sx={{
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+
+          <Box>
+            <DialogContent>
+
+
+              <Box sx={{ mt: 2 }}>
+                  <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Student Name</TableCell>
+                    <TableCell align="center">Contact</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                {stu &&
+                  stu.map((row) => (
+                    <TableBody>
+                      <TableRow
+                        key={row.name}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell align="center">{row.FullName}</TableCell>
+                        <TableCell align="center">{row.Contact}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  ))}
+              </Table>
+            </TableContainer>
+              </Box>
+            </DialogContent>
+          </Box>
+        </Box>
+      </BootstrapDialog>
+      
     </div>
   );
 };
